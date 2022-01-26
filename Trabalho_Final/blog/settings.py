8e667efbb6fec037e0fa2f0a584adfee38a97ccb
Lucas Baptista <lucasbaptistamoraes@gmail.com>
2022-01-26 19:47:50 -0300

    26/01

--- Trabalho_Final/blog/settings.py
+++ Trabalho_Final/blog/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = (BASE_DIR / 'media')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
@@ -134,7 +134,7 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = (BASE_DIR/'assets')
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
--- Trabalho_Final/blog/urls.py
+++ Trabalho_Final/blog/urls.py
@@ -24,4 +24,7 @@ urlpatterns = [
     path('', include('blog_app_redes.urls')),
     path('members/', include('django.contrib.auth.urls')),
     path('members', include('members.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
